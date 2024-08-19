67b52a60a4bd3d09d88d30947d0d4de2ebfb4c6b
raul crespo <raulxcrespo@gmail.com>
2024-08-19 18:11:31 -0300

    exercicio 3

--- exercicio03/Main.java
+++ exercicio03/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Produto produto = new Produto("Veja", "1234567890", 100.0, new Categoria("Limpeza"));
+        Produto produto = new Produto("Veja", "1234567890", 100.00, new Categoria("Limpeza"));
 
         System.out.println(produto.getNome());
         System.out.println(produto.getCodigo());
